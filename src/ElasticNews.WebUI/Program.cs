1a594fffb27f7b650b2ac3ffcadf248046a92176
Emre Kağan Erde <emrekaganerde@outlook.com>
2025-06-14 19:39:56 +0300

    add razor pages web project and configure hangfire

---
+++ src/ElasticNews.WebUI/Program.cs
@@ -0,0 +1,41 @@
+using ElasticNews.Application.Services;
+using ElasticNews.Infrastructure;
+using Hangfire;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddInfrastructure();
+
+var app = builder.Build();
+
+app.UseHangfireDashboard();
+
+RecurringJob.AddOrUpdate<INewsIndexingJob>(
+    "news-indexing-job",
+    job => job.FetchAndIndexNewsAsync(),
+    Cron.Hourly);
+
+BackgroundJob.Enqueue<INewsIndexingJob>(job => job.FetchAndIndexNewsAsync());
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
